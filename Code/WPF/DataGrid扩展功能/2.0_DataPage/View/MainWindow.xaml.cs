9a83b9f1aa0c58d76119c0cbc77ea8416d5811b3
ithsw <315157286@qq.com>
2023-09-19 17:44:23 +0800

    上班没做完，回去继续做

--- Code/WPF/DataGrid扩展功能/2.0_DataPage/View/MainWindow.xaml.cs
+++ Code/WPF/DataGrid扩展功能/2.0_DataPage/View/MainWindow.xaml.cs
@@ -10,13 +10,12 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace _2._0_DataPage
+namespace _2._0_DataPage.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// MainWindow.xaml 的交互逻辑
     /// </summary>
     public partial class MainWindow : Window
     {
