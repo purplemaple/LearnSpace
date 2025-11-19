84069b3c73b183b27723a822282ffb6d2da6b6aa
HRX <315157286@qq.com>
2025-11-19 16:41:52 +0800

    使用Obsidian管理md文档

---
+++ Code/C#进阶/多线程与异步/9_异步任务中的汇报与进度展示/1.1_ProgressForEvent/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _1._1_ProgressForEvent
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>  
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                button.IsEnabled = false;
+
+                Progress<double> progress = new(value => progressBar.Value = value);
+                await DoJobAsync(progress);
+                button.IsEnabled = true;
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        async Task DoJobAsync(IProgress<double> progress)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                await Task.Delay(50).ConfigureAwait(false);
+                progressBar.Value = i;
+                //progress.Report(i);
+            }
+        }
+    }
+}
